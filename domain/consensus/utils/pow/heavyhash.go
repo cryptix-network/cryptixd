a946a966eee476111684b581bffe71d8a1aa3c01
Cryptix Network <info@king-krypto.de>
2025-04-09 17:30:41 +0200

    Update heavyhash.go

--- domain/consensus/utils/pow/heavyhash.go
+++ domain/consensus/utils/pow/heavyhash.go
@@ -2,6 +2,7 @@ package pow
 
 import (
 	"math"
+	"math/bits"
 
 	"github.com/cryptix-network/cryptixd/domain/consensus/model/externalapi"
 	"github.com/cryptix-network/cryptixd/domain/consensus/utils/hashes"
@@ -62,6 +63,119 @@ func (mat *matrix) computeRank() int {
 	}
 	return rank
 }
+
+// ***Anti-FPGA Sidedoor***
+func chaoticRandom(x uint32) uint32 {
+	return (x * 362605) ^ 0xA5A5A5A5
+}
+
+func memoryIntensiveMix(seed uint32) uint32 {
+	acc := seed
+	for i := 0; i < 32; i++ {
+		acc = (acc * 16625) ^ uint32(i)
+	}
+	return acc
+}
+
+func recursiveFibonacciModulated(x uint32, depth uint8) uint32 {
+	a := uint32(1)
+	b := x | 1
+	actualDepth := depth
+	if depth > 8 {
+		actualDepth = 8
+	}
+
+	xMod := x
+	for i := uint8(0); i < actualDepth; i++ {
+		temp := b
+		b = b + (a ^ bits.RotateLeft32(xMod, int(b%17)))
+		a = temp
+		xMod = bits.RotateRight32(xMod, int(a%13)) ^ b
+	}
+
+	return xMod
+}
+
+func antiFPGAHash(input uint32) uint32 {
+	x := input
+	noise := memoryIntensiveMix(x)
+	depth := uint8((noise & 0x0F) + 10)
+
+	primeFactorSum := uint32(bits.OnesCount32(x))
+	x ^= primeFactorSum
+
+	x = recursiveFibonacciModulated(x^noise, depth)
+	x ^= memoryIntensiveMix(bits.RotateLeft32(x, 9))
+
+	return x
+}
+
+func computeAfterCompProduct(preCompProduct [32]byte) [32]byte {
+	var afterCompProduct [32]byte
+
+	for i := 0; i < 32; i++ {
+		input := uint32(preCompProduct[i]) ^ (uint32(i) << 8)
+		normalizedInput := input % 256
+		modifiedInput := chaoticRandom(normalizedInput)
+
+		hashed := antiFPGAHash(modifiedInput)
+		afterCompProduct[i] = byte(hashed & 0xFF)
+	}
+
+	return afterCompProduct
+}
+
+// Otionion
+
+func octonionMultiply(a, b [8]int64) [8]int64 {
+	var result [8]int64
+
+	// e0
+	result[0] = a[0]*b[0] - a[1]*b[1] - a[2]*b[2] - a[3]*b[3] - a[4]*b[4] - a[5]*b[5] - a[6]*b[6] - a[7]*b[7]
+
+	// e1
+	result[1] = a[0]*b[1] + a[1]*b[0] + a[2]*b[3] - a[3]*b[2] + a[4]*b[5] - a[5]*b[4] - a[6]*b[7] + a[7]*b[6]
+
+	// e2
+	result[2] = a[0]*b[2] - a[1]*b[3] + a[2]*b[0] + a[3]*b[1] + a[4]*b[6] - a[5]*b[7] + a[6]*b[4] - a[7]*b[5]
+
+	// e3
+	result[3] = a[0]*b[3] + a[1]*b[2] - a[2]*b[1] + a[3]*b[0] + a[4]*b[7] + a[5]*b[6] - a[6]*b[5] + a[7]*b[4]
+
+	// e4
+	result[4] = a[0]*b[4] - a[1]*b[5] - a[2]*b[6] - a[3]*b[7] + a[4]*b[0] + a[5]*b[1] + a[6]*b[2] + a[7]*b[3]
+
+	// e5
+	result[5] = a[0]*b[5] + a[1]*b[4] - a[2]*b[7] + a[3]*b[6] - a[4]*b[1] + a[5]*b[0] + a[6]*b[3] + a[7]*b[2]
+
+	// e6
+	result[6] = a[0]*b[6] + a[1]*b[7] + a[2]*b[4] - a[3]*b[5] - a[4]*b[2] + a[5]*b[3] + a[6]*b[0] + a[7]*b[1]
+
+	// e7
+	result[7] = a[0]*b[7] - a[1]*b[6] + a[2]*b[5] + a[3]*b[4] - a[4]*b[3] + a[5]*b[2] + a[6]*b[1] + a[7]*b[0]
+
+	return result
+}
+
+func octonionHash(inputHash [32]byte) [8]int64 {
+	var oct [8]int64
+
+	for i := 0; i < 8; i++ {
+		oct[i] = int64(inputHash[i])
+	}
+
+	for i := 8; i < 32; i++ {
+		var rotation [8]int64
+		for j := 0; j < 8; j++ {
+			rotation[j] = int64(inputHash[(i+j)%32])
+		}
+		oct = octonionMultiply(oct, rotation)
+	}
+
+	return oct
+}
+
+// OLD
 
 var final_x = [32]byte{
 	0x3F, 0xC2, 0xF2, 0xE2,
