98324d72d3c2a5c9cc9c19834d69e5eeffb0e53d
Cryptix Network <info@king-krypto.de>
2025-05-09 05:35:53 +0200

    Update heavyhash.go

--- domain/consensus/utils/pow/heavyhash.go
+++ domain/consensus/utils/pow/heavyhash.go
@@ -131,6 +131,10 @@ func computeAfterCompProduct(preCompProduct [32]byte) [32]byte {
 	return afterCompProduct
 }
 
+func wrapMuli64(a, b int64) int64 {
+	return int64(uint64(a) * uint64(b))
+}
+
 // Otionion
 
 // Octonion Multiply
@@ -138,28 +142,84 @@ func octonionMultiply(a, b [8]int64) [8]int64 {
 	var result [8]int64
 
 	// e0
-	result[0] = a[0]*b[0] - a[1]*b[1] - a[2]*b[2] - a[3]*b[3] - a[4]*b[4] - a[5]*b[5] - a[6]*b[6] - a[7]*b[7]
+	result[0] = wrapMuli64(a[0], b[0]) -
+		wrapMuli64(a[1], b[1]) -
+		wrapMuli64(a[2], b[2]) -
+		wrapMuli64(a[3], b[3]) -
+		wrapMuli64(a[4], b[4]) -
+		wrapMuli64(a[5], b[5]) -
+		wrapMuli64(a[6], b[6]) -
+		wrapMuli64(a[7], b[7])
 
 	// e1
-	result[1] = a[0]*b[1] + a[1]*b[0] + a[2]*b[3] - a[3]*b[2] + a[4]*b[5] - a[5]*b[4] - a[6]*b[7] + a[7]*b[6]
+	result[1] = wrapMuli64(a[0], b[1]) +
+		wrapMuli64(a[1], b[0]) +
+		wrapMuli64(a[2], b[3]) -
+		wrapMuli64(a[3], b[2]) +
+		wrapMuli64(a[4], b[5]) -
+		wrapMuli64(a[5], b[4]) -
+		wrapMuli64(a[6], b[7]) +
+		wrapMuli64(a[7], b[6])
 
 	// e2
-	result[2] = a[0]*b[2] - a[1]*b[3] + a[2]*b[0] + a[3]*b[1] + a[4]*b[6] - a[5]*b[7] + a[6]*b[4] - a[7]*b[5]
+	result[2] = wrapMuli64(a[0], b[2]) -
+		wrapMuli64(a[1], b[3]) +
+		wrapMuli64(a[2], b[0]) +
+		wrapMuli64(a[3], b[1]) +
+		wrapMuli64(a[4], b[6]) -
+		wrapMuli64(a[5], b[7]) +
+		wrapMuli64(a[6], b[4]) -
+		wrapMuli64(a[7], b[5])
 
 	// e3
-	result[3] = a[0]*b[3] + a[1]*b[2] - a[2]*b[1] + a[3]*b[0] + a[4]*b[7] + a[5]*b[6] - a[6]*b[5] + a[7]*b[4]
+	result[3] = wrapMuli64(a[0], b[3]) +
+		wrapMuli64(a[1], b[2]) -
+		wrapMuli64(a[2], b[1]) +
+		wrapMuli64(a[3], b[0]) +
+		wrapMuli64(a[4], b[7]) +
+		wrapMuli64(a[5], b[6]) -
+		wrapMuli64(a[6], b[5]) +
+		wrapMuli64(a[7], b[4])
 
 	// e4
-	result[4] = a[0]*b[4] - a[1]*b[5] - a[2]*b[6] - a[3]*b[7] + a[4]*b[0] + a[5]*b[1] + a[6]*b[2] + a[7]*b[3]
+	result[4] = wrapMuli64(a[0], b[4]) -
+		wrapMuli64(a[1], b[5]) -
+		wrapMuli64(a[2], b[6]) -
+		wrapMuli64(a[3], b[7]) +
+		wrapMuli64(a[4], b[0]) +
+		wrapMuli64(a[5], b[1]) +
+		wrapMuli64(a[6], b[2]) +
+		wrapMuli64(a[7], b[3])
 
 	// e5
-	result[5] = a[0]*b[5] + a[1]*b[4] - a[2]*b[7] + a[3]*b[6] - a[4]*b[1] + a[5]*b[0] + a[6]*b[3] + a[7]*b[2]
+	result[5] = wrapMuli64(a[0], b[5]) +
+		wrapMuli64(a[1], b[4]) -
+		wrapMuli64(a[2], b[7]) +
+		wrapMuli64(a[3], b[6]) -
+		wrapMuli64(a[4], b[1]) +
+		wrapMuli64(a[5], b[0]) +
+		wrapMuli64(a[6], b[3]) +
+		wrapMuli64(a[7], b[2])
 
 	// e6
-	result[6] = a[0]*b[6] + a[1]*b[7] + a[2]*b[4] - a[3]*b[5] - a[4]*b[2] + a[5]*b[3] + a[6]*b[0] + a[7]*b[1]
+	result[6] = wrapMuli64(a[0], b[6]) +
+		wrapMuli64(a[1], b[7]) +
+		wrapMuli64(a[2], b[4]) -
+		wrapMuli64(a[3], b[5]) -
+		wrapMuli64(a[4], b[2]) +
+		wrapMuli64(a[5], b[3]) +
+		wrapMuli64(a[6], b[0]) +
+		wrapMuli64(a[7], b[1])
 
 	// e7
-	result[7] = a[0]*b[7] - a[1]*b[6] + a[2]*b[5] + a[3]*b[4] - a[4]*b[3] + a[5]*b[2] + a[6]*b[1] + a[7]*b[0]
+	result[7] = wrapMuli64(a[0], b[7]) -
+		wrapMuli64(a[1], b[6]) +
+		wrapMuli64(a[2], b[5]) +
+		wrapMuli64(a[3], b[4]) -
+		wrapMuli64(a[4], b[3]) +
+		wrapMuli64(a[5], b[2]) +
+		wrapMuli64(a[6], b[1]) +
+		wrapMuli64(a[7], b[0])
 
 	return result
 }
